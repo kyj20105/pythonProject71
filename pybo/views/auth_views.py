104844cc95283273c47c08397c29a0732efd210d
kyj20105 <101615584+kyj20105@users.noreply.github.com>
2022-06-08 13:33:08 +0900

    Initial commit

--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, url_for, render_template, flash, request, session
+from flask import Blueprint, url_for, render_template, flash, request, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
 from pybo import db
 from pybo.forms import UserCreateForm, UserLoginForm
 from pybo.models import User
+import functools
 
 bp=Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -40,3 +41,24 @@ def login():
             return redirect(url_for('main.index'))
         flash(error)
     return render_template('auth/login.html', form=form)
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id=session.get('user_id')
+    if user_id is None:
+        g.user=None
+    else:
+        g.user=User.query.get(user_id)
+
+@bp.route('/logout/')
+def logout():
+    session.clear()
+    return redirect(url_for('main.index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
+from werkzeug.utils import redirect
 bp=Blueprint('main', __name__, url_prefix='/')
-
+from pybo.models import Question
 @bp.route('/hello')
 def hello_pybo():
     return 'Hello, Pybo!'
@@ -17,3 +18,10 @@ def drink():
 @bp.route('/dessert')
 def dessert():
     return render_template('dessert.html')
+@bp.route('question/question_list')
+def question():
+    return redirect(url_for('question._list'))
+@bp.route('/detail/<int:question_id>/')
+def detail(question_id):
+    question=Question.query.get_or_404(question_id)
+    return render_template('question/question_detail.html', question=question)
